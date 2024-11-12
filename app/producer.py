41a132efe2d860cf2ea6746b1a0dd1f5788362c9
EnoshMcCarthy <enoshh12@gmail.com>
2024-11-12 07:29:41 +0200

    pca

--- app/producer.py
+++ app/producer.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from faker import Faker
 from flask import Flask
 from kafka import KafkaProducer
+from toolz import partial
 
 fake = Faker()
 
@@ -13,10 +14,16 @@ load_dotenv(verbose=True)
 app = Flask(__name__)
 
 
+def callback(status: str, message: str):
+    print(f"{status}: {message}")
+
+
 def produce_fake_person():
     producer = KafkaProducer(
         bootstrap_servers=os.environ['BOOTSTRAP_SERVERS'],
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+        retries=int(os.environ['TOPIC_PEOPLE_BASIC_RETRIES']),
+        acks=os.environ['TOPIC_PEOPLE_BASIC_ACK']
     )
     fake_person = {
         'first_name': fake.first_name(),
@@ -25,10 +32,14 @@ def produce_fake_person():
         'address': fake.address(),
         'birth_date': fake.date_of_birth().isoformat()
     }
-    producer.send(
-        os.environ['TOPIC_PEOPLE_BASIC_NAME'],
-        value=fake_person,
-        key=fake_person['email'].encode('utf-8')
+    (
+        producer.send(
+            os.environ['TOPIC_PEOPLE_BASIC_NAME'],
+            value=fake_person,
+            key=fake_person['email'].encode('utf-8'),
+        )
+        .add_callback(partial(callback, "success"))
+        .add_errback(partial(callback, "error"))
     )
     producer.flush()
 
@@ -36,7 +47,3 @@ def produce_fake_person():
 if __name__ == '__main__':
     produce_fake_person()
     app.run()
-
-
-
-
